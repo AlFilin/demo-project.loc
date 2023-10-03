6502c6052673d656f371c5e04df05fa201576b0c
AlFilin <Filatik.al79@gmail.com>
2023-10-03 23:59:26 +0300

    main

--- themes/demo/tailwind.config.js
+++ themes/demo/tailwind.config.js
@@ -1,20 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-<<<<<<< HEAD
     'layouts/**/*.htm',
     'pages/**/*.htm',
     'partials/**/*.htm',
     "./src/**/*.{js,ts,jsx,tsx}",
     "./index.htm",
   ],
-=======
-      'layouts/**/*.htm',
-      'pages/**/*.htm',
-      'partials/**/*.htm',
-      "./src/**/*.{js,ts,jsx,tsx}"
-      ],
->>>>>>> a143e6f42abf687a15f7a6261e57dac2be161c42
   theme: {
     extend: {},
   },
