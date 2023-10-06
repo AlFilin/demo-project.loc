ab7a07335e414644de42b930b98631de4c3aab80
AlFilin <Filatik.al79@gmail.com>
2023-10-06 23:58:13 +0300

    main

--- themes/demo/tailwind.config.js
+++ themes/demo/tailwind.config.js
@@ -8,7 +8,18 @@ module.exports = {
     "./index.htm",
   ],
   theme: {
-    extend: {},
+    extend: {
+        maxWidth: {
+            'site': '1200px',
+            'container': '1140px',
+        },
+        colors: {
+            'dark-blue': '#213f50',
+            'orange': '#E67E21',
+            'blue': '#0083bb',
+            'light-yellow': '#FFF2D1',
+        }
+    },
   },
   plugins: [],
 }
